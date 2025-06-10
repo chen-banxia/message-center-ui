f7f326a7afcf7f70cae9562263b6bf79b9967900
chenzhikun <travelchen66@foxmail.com>
2025-06-10 09:54:58 +0800

    feat(package-lock): 更新依赖项和锁定文件版本
    - 将 package-lock.json 的 lockfileVersion 更新至 3
    - 重新组织依赖项结构，添加了多个新依赖项，包括 @element-plus/icons-vue、axios、echarts、element-plus、pinia、vue 和 vue-router
    - 更新了 vite 和 @vitejs/plugin-vue 的版本
    - 在 vite.config.js 中添加了开发服务器配置，指定端口为 83，并设置启动时自动打开浏览器

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 83, // 指定开发服务器端口为3000
+    open: true  // 启动时自动打开浏览器
   }
 })
